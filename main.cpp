f18c683dd93e99917a331eee2234ab6930972626
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-08-03 06:43:37 +0100

    added the neccessary dll files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include <iostream>
 
-#include <libavcodec/avcodec.h>
-#include <libavutil/opt.h>
-#include <libavutil/imgutils.h>
+extern "C" {
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+}
 
 int main(){
-    printf("wooooooow\n");
+    AVFormatContext *pFormatContext = avformat_alloc_context();
+    if(!pFormatContext){
+        std::cout << "cant allocate space for context" << std::endl;
+    }
+    if(avformat_open_input(&pFormatContext, "media/test.mp4", NULL, NULL) != 0){
+        std::cout << "cant open the file" << std::endl;
+        return 1;
+    }
+    std::cout << "successfully opened the file" << std::endl;
     return 0;
 }
--- makefile
+++ makefile
@@ -1,14 +1,23 @@
 main: main.o
-	$(info linking the app)
-	g++ main.o
+	$(info =========> linking the app <=========)
+	g++ main.o \
+	-L./libs/ffmpeg/lib \
+	-lavcodec \
+	-lavformat  \
+	-lavdevice  \
+	-lavfilter  \
+	-lavutil  \
+	-lswscale  \
+	-lswresample  \
 
 main.o: main.cpp
-	$(info compiling the app)
+	$(info =========> compiling the app <=========)
 	g++ \
 	-std=c++17 \
 	-c \
 	main.cpp \
+	-I./libs/ffmpeg/include \
 
-clean:
+clear:
 	del *.o
 	del a.exe
